4a04138e14a0c55220af88be169f89257977466c
Subham49 <109677833+Subham49@users.noreply.github.com>
2024-05-03 15:30:42 +0530

    Time: 1 ms (78.87%), Space: 40.3 MB (99.94%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] a = version1.split("\\.");
+        String[] b = version2.split("\\.");
+        int i=0, j=0;
+
+        
+
+        int n = a.length;
+        int m = b.length;
+        while(i<n && j<m)
+        {
+            int x = Integer.parseInt(a[i]);
+            int y = Integer.parseInt(b[j]);
+            // System.out.println(x+" "+y);
+            if(x>y)
+                return 1;
+            else if(x<y)
+                return -1;
+            i++;
+            j++;
+        }
+        while(i<n)
+        {
+            int x = Integer.parseInt(a[i++]);
+            if(x!=0)
+                return 1;
+        }
+
+        while(j<m)
+        {
+            int x = Integer.parseInt(b[j++]);
+            if(x!=0)
+                return -1;
+        }
+        return 0;
+    }
+}
