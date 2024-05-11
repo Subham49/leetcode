9cd299cabb17be76d95c115843e351bcf3e246a7
Subham49 <109677833+Subham49@users.noreply.github.com>
2024-05-11 11:50:20 +0530

    Time: 1 ms (99.76%), Space: 42.3 MB (71.22%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -17,7 +17,42 @@ class Solution {
         }
         return ans;
     }
+    int withoutStackApproach(String s)
+    {
+        int o = 0;
+        int c = 0;
+        int ans = 0;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(') o++;
+            else c++;
+
+            if(o == c)
+            {
+                ans = Math.max(ans, o+c);
+            }
+            else if(c>o){
+                o = c = 0;
+            }
+        }
+
+        o = c = 0;
+        for(int i=s.length()-1; i>=0; i--){
+            char ch = s.charAt(i);
+            if(ch == '(') o++;
+            else c++;
+            
+            if(o == c)
+            {
+                ans = Math.max(ans, o+c);
+            }
+            else if(c<o){
+                o = c = 0;
+            }
+        }
+        return ans;
+    }
     public int longestValidParentheses(String s) {
-        return stackApproach(s);
+        return withoutStackApproach(s);
     }
 }
