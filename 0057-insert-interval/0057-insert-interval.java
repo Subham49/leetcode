52e95c49c53ec12b632a4b18cf76ae916f7e2a90
Subham49 <109677833+Subham49@users.noreply.github.com>
2024-05-21 23:20:52 +0530

    Time: 2 ms (43.19%), Space: 45 MB (13.33%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public int[][] merge(int[][] intervals) {
+        ArrayList<int[]> list = new ArrayList<>();
+        Arrays.sort(intervals, (a, b)->a[0]-b[0]);
+        list.add(intervals[0]);
+        
+        for(int i=1; i<intervals.length; i++)
+        {
+            int end = list.get(list.size()-1)[1];
+            int start = intervals[i][0];
+            if(start<=end)
+            {
+                list.get(list.size()-1)[1] = Math.max(list.get(list.size()-1)[1], intervals[i][1]);
+            }
+            else
+            {
+                list.add(intervals[i]);
+            }
+        }
+        
+        int ans[][] = new int[list.size()][2];
+        for(int i=0; i<list.size(); i++)
+        {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int arr[][] = Arrays.copyOf(intervals, intervals.length + 1);
+        arr[intervals.length] = newInterval;
+
+        return merge(arr);
+    }
+}
