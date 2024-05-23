b8cfeb40c96ccecca9702e9160ef1d11690275f3
Subham49 <109677833+Subham49@users.noreply.github.com>
2024-05-24 00:04:01 +0530

    Time: 0 ms (100%), Space: 41.6 MB (62.58%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int a[] = new int[3];
+        for(int i : nums) a[i]++;
+        int k=0;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<a[i]; j++) nums[k++] = i;
+        }
+    }
+}
